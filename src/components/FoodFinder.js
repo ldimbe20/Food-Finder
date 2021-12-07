430b6e10fa5ec3ee83455ffb3dcb5cb81267be35
ldimbe20 <ldimbe20@gmail.com>
2021-12-07 09:30:04 -0600

    Added Authorization Page

--- src/components/FoodFinder.js
+++ src/components/FoodFinder.js
@@ -1,15 +1,35 @@
-import React, { useEffect, useState } from "react"
-import { NavBar } from "./nav/NavBar";
+// import React, { useEffect, useState } from "react"
 import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import { Route, Redirect} from "react-router-dom"
 
-export const FoodFinder = () => {
-    
-    return (
-        <>
-            <NavBar />
-            <h1>Food Finder</h1>
-            <ApplicationViews />
-        </>
-    )
 
-}
+export const FoodFinder = () => (
+    <>
+      <Route
+        render={() => {
+          if (localStorage.getItem("honey_customer")) {
+            return (
+                <>
+                <NavBar />
+                <h1>Food Finder</h1>
+                <ApplicationViews />
+            </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+  
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </>
+  );
+
