c15451fa2222b717d2363dcafdede26de3def57a
ldimbe20 <ldimbe20@gmail.com>
2021-12-16 14:27:22 -0600

    AllergyForm is working and displaying profile

--- src/components/friends/FriendsProfileList.js
+++ src/components/friends/FriendsProfileList.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react"
-import { useHistory, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 
 
 
 export const FriendProfileList = () => {
     const [friends, setFriends] = useState([])
-    const history = useHistory()
+    
 
     useEffect( //instead of calling a fetch call to display change here we are invoking the getTickets function which does it for us 
         () => {
@@ -15,7 +16,7 @@ export const FriendProfileList = () => {
     ) //invoking getTickets 
 
     const getFriends = () => {  //this function is used to replace data information after ticket has been deleted
-        return fetch("http://localhost:8088/friends")
+        return fetch("http://localhost:8088/friendFoodRestrictions?_expand=severity&_expand=friend")
             .then(res => res.json())
             .then((data) => {
                 setFriends(data)
@@ -32,10 +33,11 @@ export const FriendProfileList = () => {
                         <h1>THis is the friends profile list</h1>
                         return <div key={`friend--${friend.id}`}>
                                 {/* above states if className is emergency is friend.emergy is true and is friend if friend.emergency is false */}
-                                <h4> {friend.name} 
+                                <h4> {friend?.friend?.name} 
                                 </h4>
-                                <button onClick={() => {
-                                }}><Link to={`/allergies`}>Add Allergy</Link></button>
+                                <p>is allergic to {friend.foodRestrictionName}</p>
+                                <p>Her allergic reaction is {friend?.severity?.level}</p>
+                                
                             </div>
                     }
                 )
