c43aa63cf2e9c7b07268755d9ccd9f2c73af2bc7
ldimbe20 <ldimbe20@gmail.com>
2021-12-15 19:05:21 -0600

    Added dropdown to AllergyForm

--- src/components/allergies/AllergiesForm.js
+++ src/components/allergies/AllergiesForm.js
@@ -6,59 +6,100 @@ export const AllergiesForm = () => {   //restrictionForm is a component
     const history = useHistory()
 
 
-    const [foodRestrictionName, setfoodRestrictionName] = useState({
-        userId: parseInt(localStorage.getItem("food_customer"))  //how do I get the friend id
+    const [severity, setSeverity] = useState({
+          
     })
 
+    const [foodRestrictionName, setfoodRestrictionName] = useState({
+        userId: parseInt(localStorage.getItem("food_customer"))  
+    })
+   
+   
 
+    const history =  useHistory() //creating a variable that equals the return of useHistory
 
-
-    const saveAllergies = (evt) => {
+    const saveAllegy = (evt) => {  //this function is using the state, useState, to create an object to store to API
         evt.preventDefault()
-        const fetchOption = (parameter) => {
-            return {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(parameter)
-            }
+        const newAllergy ={  //creating an object to save 
+            foodRestrictionName: foodRestrictionName.foodName,  //getting information form state 
+            friendId:1,
+            severityId: 1
+    } //we want to send above object to API
+   
+   const fetchOption = {
+            method: "POST", //posting to the API using POst method
+            headers: {
+                "Content-Type": "application/json" 
+           },
+           body:JSON.stringify(newAllergy) // sending the body of the object new foodRestrictionName, can't send javascript so need to stringify 
         }
 
-        return fetch("http://localhost:8088/friendFoodRestrictions", fetchOption(foodRestrictionName)) //fetchOption is a function on line 34 which post parameter (foodRestrictionName) to api
-            .then(response => response.json())//the response,which is profile name value, is gathered and turned to javascript
-            .then((storedResponse) => { //the response is then converted to storedResponse
+         return fetch ("http://localhost:8088/friendFoodRestrictions", fetchOption)
+            .then(response => response.json())
+        //     .then (() => {
+        //         history.push("/foodRestrictionNames")  
+        // })
+
+   }
+
+   useEffect( // a hook whose responsibility it is to react to change state
+    () => {
+        fetch("http://localhost:8088/severity")
+            .then(res => res.json())
+            .then((data) => {
+                setSeverity(data)
             })
-        .then((data) => {   
-                history.push("/profileList")
-        }) 
-    }
+    },
+    [] //
+)
+   
+   
+    
+
 
     return (
-        <form className="userRestrictionForm">
-            <h2 className="userRestrictionForm__title">New Food Restriction Profile</h2>
+        <>
+            <form className="allergyForm">
+                <h2 className="allergyForm__title">New Service FoodRestrictionName</h2>
+                <fieldset>
+                    <div className="form-group">
+                        <label htmlFor="foodName">Description:</label>
+                        <input
+                            required autoFocus
+                            type="text"
+                            className="form-control"
+                            placeholder="Brief description of problem"
+                            onChange={ //onChange is like an event listener that listens for a change and records it- we are listening for the change in description here
+                                (evt) => {
+                                    const copy = { ...foodRestrictionName }     //using object spread operator to copy the initual state
+                                    copy.description = evt.target.value  //making the new description = the value of someone typing into the description field
+                                    setfoodRestrictionName(copy)
+                                }
+                            } />
+                        {/* above isnow that copy is updated with new data it will take that copy to update from useState hook */}
+                    </div>
+                </fieldset>
+                <fieldset>
+                    {
+                        severity.map(
+                            (severityLevel) => {
+                                {
+                                    return <option key="instrument_dropdown" value={severityLevel.id}>
+                                        {severityLevel.level}
+                                    </option>
 
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="friend">List Allergies:</label>
-                    <input
-                        required autoFocus
-                        type="text"
-                        className="form-control"
-                        placeholder="List Allergies"
-                        onChange={ //onChange is like an event listener that listens for a change and records it- we are listening for the change in description here
-                            (evt) => {
-                                const copy = { ...foodRestrictionName }     //using object spread operator to copy the initual state
-                                copy.foodRestrictionName = evt.target.value  //making the new description = the value of someone typing into the description field
-                                setfoodRestrictionName(copy)
+                                }
+
+
                             }
-                        } />
-                </div>
-            </fieldset>
+                        )
+                    }
 
-            <button className="btn btn-primary" onClick={(evt) => saveAllergies(evt)}>
-                Save Profile
-            </button>
-        </form>
+                </fieldset>
+                <button className="btn btn-primary" onClick={saveAllergy}>
+                    Submit FoodRestrictionName
+                </button>
+            </form>
+        </>
     )
 }
