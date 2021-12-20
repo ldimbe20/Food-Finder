c1565b82a7fab474d51f82e3206d7423044ae5ec
ldimbe20 <ldimbe20@gmail.com>
2021-12-20 10:01:05 -0600

    Added CSS to register form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react"
 import { useHistory } from "react-router-dom"
-import "./Login.css"
+import "./Register.css"
 
 export const Register = (props) => {
     const [user, setUser] = useState({})
@@ -66,7 +66,7 @@ export const Register = (props) => {
                     <input onChange={updateUser} type="email" id="email" className="form-control" placeholder="Email address" required />
                 </fieldset>
                 <fieldset>
-                    <button type="submit"> Register </button>
+                    <button className="Register" type="submit"> Register </button>
                 </fieldset>
             </form>
         </main>
---
+++ src/components/severityLevel/SeverityLevel.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+export const Severities = () => {
+
+
+    return (
+        <>
+        <div className="Severities">
+            <h2 className="userRestrictionForm__title">Create New Profile</h2>
+            <p> THIS IS testing</p>
+    
+        </div>
+     </>
+    )
+}
