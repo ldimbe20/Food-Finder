102ac5dcff45bbc8bcf03b30a6108a2f043bfb56
ldimbe20 <ldimbe20@gmail.com>
2022-01-29 15:09:31 -0600

    Fixed functionality in food severity tab. People with the food severity level are appearing on thier level.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,27 +5,29 @@ import "./Welcome.css";
 export const Welcome = () => {
     const history = useHistory()
     return (
-        <> 
-         <h2>Forbidden Food Finder</h2>
-         <h4 className="welcome">Welcome to Forbidden Food Finder
-        where you can see your friendʼs and
-        familyʼs dietary needs.</h4>
- 
-                     <div className="centerChildren btn--newResource">
-                            <button type="button"
-                                className="btn btn-success "
-                                onClick={() => { history.push("/restrictions/create") }}>
-                                Make A New Profile
-                            </button>
-                    </div>
+        <>
+            <body className="Image_body">
+                <h2>Forbidden Food Finder</h2>
+                <h4 className="welcome">Welcome to Forbidden Food Finder
+                    where you can see your friendʼs and
+                    familyʼs dietary needs.</h4>
 
-                    <div className="centerChildren btn--newResource">
-                            <button type="button"
-                                className="btn btn-success "
-                                onClick={() => { history.push("/profileList") }}>
-                                See Profiles
-                            </button>
-                    </div>
+                <div className="centerChildren btn--newResource">
+                    <button type="button"
+                        className="btn btn-success "
+                        onClick={() => { history.push("/restrictions/create") }}>
+                        Make A New Profile
+                    </button>
+                </div>
+
+                <div className="centerChildren btn--newResource">
+                    <button type="button"
+                        className="btn btn-success "
+                        onClick={() => { history.push("/profileList") }}>
+                        See Profiles
+                    </button>
+                </div>
+            </body>
         </>
     )
 }
--- src/components/severityLevel/SeverityLevel.js
+++ src/components/severityLevel/SeverityLevel.js
@@ -1,161 +1,82 @@
+
+
+
 import React, { useEffect, useState } from "react"
 import "./SeverityLevel.css"
 
 
 
 export const Severities = () => {
-    const [friendFoodRestriction, setfriendFoodRestriction] = useState([])
-    const [totalSeverityMessage, UpdateSeverityMessage] = useState([])
-    const [totalSeverityMessageThree, UpdateSeverityMessageThree] = useState("")
-    const [totalSeverityMessageTwo, UpdateSeverityMessageTwo] = useState("")
-    const [totalSeverityMessageOne, UpdateSeverityMessageOne] = useState([])
+    const loggedInId = parseInt(localStorage.getItem("food_customer"));
+    const [severityModel, setSeverityModel] = useState([])
+    const [severityLevels, setSeverityLevels] = useState([])
+    const [friendFoodRestriction, setFriendFoodRestriction] = useState([])
+    
 
 
 
+    console.log("before useEffect");
     useEffect( // Getting data used to set severity level in next useEffect
         () => {
             fetch("http://localhost:8088/friendFoodRestrictions?_expand=friend&_expand=severityLevel")
                 .then(res => res.json())
-                .then((data) => {
-                    setfriendFoodRestriction(data) //taking data as argument
+                .then((data) => { 
+                    // console.log("friendFoodRestrictions");
+                    // console.log(data);
+                    setFriendFoodRestriction(data);
+                    fetch("http://localhost:8088/severityLevels")
+                        .then(resSeverity => resSeverity.json())
+                        .then((dataSeverity) => {
+                            setSeverityLevels(dataSeverity);
+
+                           //!create new data that includes restrictions that filter food restrictions by if user is logged in and by food severity level
+                           console.log("friendFoodRestrictions", friendFoodRestriction)
+                    
+                           setSeverityModel(dataSeverity.map((severityLevel) => {
+                                return {
+                                    key: severityLevel.id,
+                                    severityId: severityLevel.id,
+                                    severityDisplayName: `Level ${severityLevel.id}`,
+                                    description: severityLevel.description,
+                                    restrictions: data.filter(restriction => restriction.friend?.userId == loggedInId && restriction.severityLevelId === severityLevel.id)
+                               //above is coming back with an empty array but only sometimes
+                               
+                                };
+                            }));
+                        });
+
                 })
         },
-        [] //
+        []
     )
 
-    useEffect(
-        () => {
-            friendFoodRestriction.filter((friend) => {
-
-                if (friend.severityLevelId === 4) {
-                    UpdateSeverityMessage(`This allergy severity level affects ${friend.friend.name}`)
-                }
-
-            })
-
-        },
-        [friendFoodRestriction] 
-    )
-
-
-
-
-    useEffect(
-        () => {
-            friendFoodRestriction.map((friend) => {
-                if (friend.severityLevelId === 3) {
-                    UpdateSeverityMessageThree(`This allergy severity level affects ${friend.friend.name} `)
-                }
-
-            })
-
-        },
-        [friendFoodRestriction] //
-    )
-
-    useEffect(
-        () => {
-            friendFoodRestriction.map((friend) => {
-                if (friend.severityLevelId === 2) {
-                    UpdateSeverityMessageTwo(`This allergy severity level affects ${friend.friend.name}`)
-                }
-
-            })
-
-        },
-        [friendFoodRestriction] //
-    )
-
-
-    useEffect(
-        () => {
-            friendFoodRestriction.map((friend) => {
-                if (friend.severityLevelId === 1) {
-                    UpdateSeverityMessageOne(`This allergy severity level affects ${friend.friend.name}`)
-                }
-
-            })
-
-        },
-        [friendFoodRestriction] //
-    )
-
-
-    const loggedIn = parseInt(localStorage.getItem("food_customer"));
     return (
         <>
-            {
-                friendFoodRestriction.map(
-                    (friend) => {
-                        if (loggedIn === friend.friend.userId) {
-                            if (friend.severityLevelId === 4) {
-                                return (
-                                    <div className="Source">
-                                        <>  <h3 className="Level"><b>Level 4</b></h3>
-                                            <p className="Description">
-                                                Hives, itching or eczema. Swelling of the lips, face, tongue and throat or other parts of the body
-                                                Wheezing, nasal congestion or trouble breathing</p>
-                                            <h4 className="Treatment">Treatment:</h4>
-                                            <p className="Description">Use epinephrine. This medicine is injected under the skin or into the muscle of your outer thigh only. Do not inject this medicine into a vein, into the muscle of your buttocks, or into your fingers, toes,
-                                                hands, or feet. To do so, may increase the chance of having serious side effects. <b>SEEK EMERGENCY ASSISTANCE</b></p>
-                                            <p className="people__Allergy">
-                                                {totalSeverityMessage} </p>
-                                        </>
-                                    </div>
-                                )
-                            }
-                            else if (friend.severityLevelId === 3) {
-                                return (
-                                    <div className="Source">
-                                        <>   <h3 className="Level"><b>Level 3</b></h3>
-                                            <p className="Description">
-                                                Hives, itching or eczema. Swelling of the lips, face, tongue and throat or other parts of the body
-                                                Wheezing, nasal congestion or trouble breathing</p>
-                                            <h4 className="Treatment">Treatment:</h4>
-                                            <p className="Description">Depending on severity use epinephrine. This medicine is injected under the skin or into the muscle of your outer thigh only. Do not inject this medicine into a vein, into the muscle of your buttocks, or into your
-                                                fingers, toes, hands, or feet. To do so, may increase the chance of having serious side effects. </p>
-                                            <p className="people__Allergy">
-                                                {totalSeverityMessageThree} </p>
-                                        </>
-                                    </div>
-                                )
-                            }
-                            else if (friend.severityLevelId === 2) {
-                                return (
-                                    <div className="Source">
-                                        <>
-                                            <h3 className="Level"><b>Level 2</b></h3>
-                                            <p className="Description">
-                                                Hives, itching or eczema. Swelling of the lips, face, tongue and throat or other parts of the body</p>
-                                            <h4 className="Treatment">Treatment:</h4>
-                                            <p className="Description">Depending on severity use common allergy medicines such as benedryl, antihistamines, adrenaline. Adrenaline may be needed. </p>
-                                            <p className="people__Allergy">{totalSeverityMessageTwo} </p>
-                                        </>
-                                    </div>
-                                )
-                            }
-                            else if (friend.severityLevelId === 1) {
-                                return (
-                                    <div className="Source">
-                                        <>  <h3 className="Level"><b>Level 1</b></h3>
-                                            <p className="Description">Tingling or itching in the mouth.
-                                                Swelling of the lips, face, tongue.</p>
-                                            <h4 className="Treatment">Treatment:</h4>
-                                            <p className="Description">Use common allergy medicines such as benedryl, antihistamines, adrenaline.</p>
-                                            <p className="people__Allergy">{totalSeverityMessageOne} </p>
-                                        </>
-                                    </div>
-                                )
-                            }
-
-                        }
+            <h2 className="allergyForm__title">Understanding Food Allergy Levels</h2>
+            { severityModel.map(
+                    (severityItem) => {
+                        return (
+                            
+                            <div className="Source" key={`severityItem--${severityItem.key}`}>
+                                <>  <h3 className="Level" key={`displayName--${severityItem.severityDisplayName}`}><b>{severityItem.severityDisplayName}</b></h3>
+                                    
+                                    <p className="Description" key={`description--${severityItem.description}`}>
+                                    <b>Common Symptoms: </b>{severityItem.description}
+                                    </p>
+                                    <b><i>This allergy level affects </i></b>
+                                    <p className="people__Allergy2" key={`restrictions--${severityItem.restrictions}`}>
+                                    
+                                     {severityItem.restrictions.map((r) => {return r.friend.name}).join(", ")} </p> 
+                                       
+                                </>
+                            </div>
+                        )
                     }
                 )
             }
 
-
         </>
-    )
+    );
 
 }
 
